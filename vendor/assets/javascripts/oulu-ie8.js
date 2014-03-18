5c803f1be57396210b554e038a2198b92290840e
teppei machida <machidanohimitsu@gmail.com>
2014-03-18 10:52:35 +0900

    Update

--- vendor/assets/javascripts/oulu-ie8.js
+++ vendor/assets/javascripts/oulu-ie8.js
@@ -1,5 +1,6 @@
 //= require oulu/ie/jquery.linkscubber
 //= require oulu/ie/jquery.belatedPNG.min
 //= require oulu/ie/PIE_IE678
-//= require oulu/ie/ie-selectors
+//= require oulu/ie/jquery.linkscubber
+//= require oulu/ie/svgeezy.min
 //= require oulu/ie/ie8-selectors
