3db3070ba2d97ead551a0e3a6528203a4f09d920
teppei machida <machida@fjord.jp>
2014-06-19 16:38:59 +0900

    Fix rem

--- lib/oulu-rails.rb
+++ lib/oulu-rails.rb
@@ -6,7 +6,6 @@ module OuluRails
     config.compass.require "rgbapng"
     config.compass.require 'SassyLists'
     config.compass.require 'ceaser-easing'
-    config.compass.require 'breakpoint'
     config.compass.require 'modular-scale'
     config.require 'sassy-maps'
   end
