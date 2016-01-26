760acf85adf64fcedf661019b2e9966c43c50e19
teppei machida <machida@fjord.jp>
2016-01-26 17:00:17 +0900

    Remove ruby function

--- lib/oulu-rails.rb
+++ lib/oulu-rails.rb
@@ -6,20 +6,3 @@ module OuluRails
   class Engine < ::Rails::Engine
   end
 end
-
-module AddFunctions
-  def remove_selector(string)
-    assert_type string, :String
-    return Sass::Script::String.new(string.value.gsub(/(\.|#)/, ""))
-  end
-  def get_unicode(string)
-    assert_type string, :String
-    Sass::Script::String.new(string.value.codepoints.map{ |i|
-        '\\' + i.to_s(16).upcase
-    }.join(''), :string)
-  end
-end
-
-module Sass::Script::Functions
-  include AddFunctions
-end
